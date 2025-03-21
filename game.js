868aaa6c0021a8e7ec72fd402e2116494cbf6a33
Hamza <199155278+hsale007@users.noreply.github.com>
2025-03-21 13:53:11 -0400

    Finished Game UI

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let gameOver = false;
 
 function getComputerChoice() {
   let guess = Math.floor(Math.random() * 3);
@@ -17,62 +18,71 @@ function getHumanChoice() {
   return userChoice;
 }
 
-function getWinner(humanChoice, computerChoice) {
+function getRoundWinner(humanChoice, computerChoice) {
+  const results = document.querySelector(".round-results");
   if (humanChoice === computerChoice) {
-    console.log("Tied! You both played " + humanChoice);
+    results.textContent = "Tied! You both played " + humanChoice;
   } else if (
     (humanChoice === "Rock" && computerChoice === "Scissors") ||
     (humanChoice === "Scissors" && computerChoice === "Paper") ||
     (humanChoice === "Paper" && computerChoice === "Rock")
   ) {
     humanScore++;
-    console.log("You Win! " + humanChoice + " beats " + computerChoice);
+    results.textContent =
+      "You Win! " + humanChoice + " beats " + computerChoice;
   } else {
     computerScore++;
-    console.log("You Lose! " + computerChoice + " beats " + humanChoice);
+    results.textContent =
+      "You Lose! " + computerChoice + " beats " + humanChoice;
   }
+
+  if (humanScore === 5 || computerScore === 5) getWinner();
+}
+
+function getWinner() {
+  const winner = document.querySelector(".winner");
+  if (humanScore > computerScore) {
+    winner.textContent = "Yay You Win!";
+  } else {
+    winner.textContent = "Computer Wins!";
+  }
+  gameOver = true;
+  playAgain();
+}
+
+function playAgain() {
+  const replayButton = document.createElement("button");
+  replayButton.textContent = "PLAY AGAIN?";
+  document.querySelector(".scores").appendChild(replayButton);
+
+  replayButton.addEventListener("click", () => {
+    location.reload();
+  });
+}
+
+function updateScores() {
+  const computerScoreElement = document.querySelector(".computer-score > span");
+  const humanScoreElement = document.querySelector(".human-score > span");
+  computerScoreElement.textContent = computerScore;
+  humanScoreElement.textContent = humanScore;
 }
 
 function playRound(humanChoice, computerChoice) {
   humanChoice =
     humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+  const humanChoiceElement = document.querySelector(".human-choice");
+  const computerChoiceElement = document.querySelector(".computer-choice");
 
-  console.log("You selected " + humanChoice);
-  console.log("Computer selected " + computerChoice);
-  getWinner(humanChoice, computerChoice);
-  console.log("Computer Score: " + computerScore);
-  console.log("Your Score: " + humanScore);
-  console.log();
+  humanChoiceElement.textContent = "You selected " + humanChoice;
+  computerChoiceElement.textContent = "Computer selected " + computerChoice;
+  getRoundWinner(humanChoice, computerChoice);
+  updateScores();
 }
 
-const rock = document.querySelector("button.rock");
-const paper = document.querySelector("button.paper");
-const scissors = document.querySelector("button.scissors");
 const playerChoices = document.querySelectorAll("button");
 
 playerChoices.forEach((choice) => {
-  choice.addEventListener("click", () => {
-    console.log(choice.className);
+  choice.addEventListener("click", (e) => {
+    if (!gameOver) playRound(choice.className, getComputerChoice());
   });
 });
-
-/*
-function playGame() {
-  for (let i = 1; i <= 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-
-  console.log("Computer Score: " + computerScore);
-  console.log("Your Score: " + humanScore);
-  if (humanScore === computerScore) {
-    console.log("Tied");
-  } else if (humanScore > computerScore) {
-    console.log("Yay You Win!");
-  } else {
-    console.log("Computer Wins!");
-  }
-}
-
-playGame();
-
-*/
